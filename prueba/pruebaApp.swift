48f0d11edb4f20fdcb4afe1d95314c078e060da6
Wiliam Ochoa <wochoa@regalar.tech>
2023-07-06 11:01:37 -0600

    Initial Commit

---
+++ prueba/pruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaApp.swift
+//  prueba
+//
+//  Created by Wiliam Ochoa on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct pruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
